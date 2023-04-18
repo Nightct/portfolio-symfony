88639be116715bb20b6b20d4ab7561c8692b5990
Nightct <thib-ct@hotmail.fr>
2023-04-18 02:00:24 +0200

    first update

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+let scrollBar = document.querySelector('.scroll-bar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+    scrollIndicator();
+}
+
+function scrollIndicator() {
+    let maxHeight = window.document.body.scrollHeight - window.innerHeight;
+    let percentage = ((window.scrollY) / maxHeight) * 100;
+    scrollBar.style.width = percentage + '%';
+}
